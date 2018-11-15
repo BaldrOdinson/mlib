b66c4cfa4f2404e4efe6ef68a32573bf17b471ba
baldrodinson <sheroukhov@gmai.com>
2018-11-15 17:07:35 +0300

    Add some func esc

--- mlibsite/methodics/text_formater.py
+++ mlibsite/methodics/text_formater.py
@@ -1,5 +1,10 @@
 import os
+
 def text_format_for_html(text):
+    '''
+    Разбиваем полученный из базы текст по строкам, чтобы затем вывести в форме в отдельных <p>
+    Иммитация переносов строк, которые иначе проебываются, а <pre> не поддерживает bootstrap styling
+    '''
     with open('text_tmp.txt', 'w') as file:
         file.write(text)
     html_text_list=[]
